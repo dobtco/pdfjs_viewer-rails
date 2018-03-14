8f4779cfa682df2a6ceb21971c846727a9a9c24b
Victor Lymar <evidenceofllama@gmail.com>
2018-03-14 15:14:26 -0700

    Send both XFO and CSP headers. Adds documentation too.

--- app/controllers/pdfjs_viewer/viewer_controller.rb
+++ app/controllers/pdfjs_viewer/viewer_controller.rb
@@ -19,11 +19,20 @@ module PdfjsViewer
       # By default Rails sends the 'X-Frame-Options: SAMEORIGIN' header with responses.
       # This means that the response can only be rendered on an iframe whose parent has the same
       # origin as the response.
-      # Chrome doesn't support the ALLOW-FROM directive so we can't use X-Frame-Options
-      # to whitelist just our intended origin.
-      # Instead we just remove the X-Frame-Options header altogether so from the browser perspective
-      # the repsonse can be embedded on any page.
-      response.headers.except! 'X-Frame-Options'
+
+      # We are mounting this engine under a different subdomain from the main app, so the default Rails
+      # XFO header breaks the feature. The purpose of this method is to allow pdfs to be embedded
+      # by a specific domain (the screendoor domain).
+
+      # There are two ways to control which domains are allowed to embed documents as iframes:
+      # X-Frame-Options and Content-Security-Policy. XFO is considered obsolete with CSP.
+      # XFO offers the ALLOW-FROM directive, which only allows you to specify one domain. Chrome
+      # actively ignores this directive! CSP offers frame-ancestors which lets us provide a list
+      # of domains. Only modern browsers support CSP (read: not IE/edge)
+      # So we will send both.
+
+      response.headers['X-Frame-Options'] = "ALLOW-FROM #{::Rails.configuration.x.host_with_protocol}"
+      response.headers['Content-Security-Policy'] = "frame-ancestors #{::Rails.configuration.x.host_with_protocol}"
     end
   end
 end
