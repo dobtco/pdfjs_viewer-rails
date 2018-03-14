029befe711468ffa286d55c479b94310f0b4e6c9
Victor Lymar <evidenceofllama@gmail.com>
2018-03-14 13:36:47 -0700

    Strip X-Frame-Options headers so that pdf can be rendered on a different origin.
    Pdfs will be rendered from attachments.dobt.co, and that iframe will be embedded
    in screendoor.dobt.co.

--- app/controllers/pdfjs_viewer/viewer_controller.rb
+++ app/controllers/pdfjs_viewer/viewer_controller.rb
@@ -2,13 +2,28 @@ module PdfjsViewer
   class ViewerController < PdfApplicationController
     layout false
 
+    after_action :allow_embedding_in_iframe
+
     def full
     end
 
     def minimal
     end
-    
+
     def reduced
+    end
+
+    private
+
+    def allow_embedding_in_iframe
+      # By default Rails sends the 'X-Frame-Options: SAMEORIGIN' header with responses.
+      # This means that the response can only be rendered on an iframe whose parent has the same
+      # origin as the response.
+      # Chrome doesn't support the ALLOW-FROM directive so we can't use X-Frame-Options
+      # to whitelist just our intended origin.
+      # Instead we just remove the X-Frame-Options header altogether so from the browser perspective
+      # the repsonse can be embedded on any page.
+      response.headers.except! 'X-Frame-Options'
     end
   end
 end
